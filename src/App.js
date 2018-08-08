33e2668691e3f692d2e399ff86a1abac4938b2de
Rashiduddin Yoldash <Rashid@kayten.com.tr>
2018-08-08 14:33:36 +0300

    19- Exercise- Decrement Button my Solution

--- src/App.js
+++ src/App.js
@@ -36,6 +36,15 @@ class App extends Component {
     counters[index].value++;
     this.setState({ counters });
   };
+
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = {...counter};
+    counters[index].value--;
+    this.setState({counters});
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -44,6 +53,7 @@ class App extends Component {
           <Counters 
             counters={this.state.counters}
             onIncrement={this.handleIncrement} 
+            onDecrement={this.handleDecrement}
             onDelete={this.handleDelete} 
             onReset={this.handleReset} />
         </main>
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,20 +3,31 @@ import React, { Component } from "react";
 class Counter extends Component {
   render() {
     return (
-      <div>
-        <span className={this.getBadgesClass()}>{this.formatCount()}</span>
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
-        >
-          Increment
-        </button>
-        <button
-          className="btn btn-danger btn-sm m-2"
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-        >
-          Delete
-        </button>
+      <div className="row">
+        <div className="col-sm">
+          <span className={this.getBadgesClass()}>{this.formatCount()}</span>
+        </div>
+        <div className="col-sm">
+          <button
+            onClick={() => this.props.onIncrement(this.props.counter)}
+            className="btn btn-secondary btn-sm m-2"
+          >
+            +
+          </button>
+          <button
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            className="btn btn-secondary btn-sm"
+            disabled={this.props.counter.value === 0}
+          >
+            -
+          </button>
+          <button
+            className="btn btn-danger btn-sm m-2"
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+          >
+            X
+          </button>
+        </div>
       </div>
     );
   }
