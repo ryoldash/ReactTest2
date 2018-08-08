6f903eb21f0b0052a64268bee9930428500776db
Rashiduddin Yoldash <Rashid@kayten.com.tr>
2018-08-08 13:46:49 +0300

    12- Lifting the State Up example

--- src/App.js
+++ src/App.js
@@ -4,12 +4,48 @@ import Counters from "./components/counters";
 import NavBar from "./components/navBar";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 5 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(
+      counter => counter.id !== counterId
+    );
+    this.setState({ counters: counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters]; //the spread operator is used on array
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter }; //the spread operator is used on object
+    counters[index].value++;
+    this.setState({ counters });
+  };
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
         <main className="container">
-          <Counters />
+          <Counters 
+            counters={this.state.counters}
+            onIncrement={this.handleIncrement} 
+            onDelete={this.handleDelete} 
+            onReset={this.handleReset} />
         </main>
       </React.Fragment>
     );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,53 +2,20 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 5 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(
-      counter => counter.id !== counterId
-    );
-    this.setState({ counters: counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0;
-      return c;
-    });
-
-    this.setState({ counters });
-  };
-
-  handleIncrement = counter => {
-    const counters = [...this.state.counters]; //the spread operator is used on array
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter }; //the spread operator is used on object
-    counters[index].value++;
-    this.setState({ counters });
-  };
-
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
           />
         ))}
