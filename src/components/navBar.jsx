5681650a5121314dff197030f0aeb18e858ad2e4
Rashiduddin Yoldash <Rashid@kayten.com.tr>
2018-08-08 14:01:07 +0300

    14- Destructuring Arguments example

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,17 +2,15 @@ import React, { Component } from "react";
 
 // Stateless Functional Components
 
-const NavBar = (props) => {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar{" "}
-          <span className="badge bage-pill badge-secondary">
-            {props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-}
- 
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge bage-pill badge-secondary">{totalCounters}</span>
+      </a>
+    </nav>
+  );
+};
+
 export default NavBar;
